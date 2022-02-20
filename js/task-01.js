3dc02c0c8d348144efef537d049071f24d303680
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-02-20 23:27:08 +0400

    fixed task 1 forEach

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,9 @@ const categories = document.querySelector("#categories");
 const categoriesArr = categories.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesArr.length}`);
 
-for (let i = 0; i < categoriesArr.length; i += 1) {
-  console.log(`Category: ${categoriesArr[i].querySelector("h2").textContent}`);
+categoriesArr.forEach(function (elemnt) {
   console.log(
-    `Elements: ${categoriesArr[i].querySelector("ul").children.length}`
+    `Category: ${elemnt.querySelector("h2").textContent}
+Elements: ${elemnt.querySelector("ul").children.length}`
   );
-}
+});
