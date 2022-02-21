2ade05fa86820b56939b1617d9b22ddc85b4aa8f
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-02-21 20:16:15 +0400

    fixed  callback

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const categories = document.querySelector("#categories");
-const categoriesArr = categories.querySelectorAll(".item");
+// const categories = document.querySelector("#categories");
+const categoriesArr = document.querySelector("#categories").querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesArr.length}`);
 
-categoriesArr.forEach(function (elemnt) {
+categoriesArr.forEach( (elemnt) => {
   console.log(
     `Category: ${elemnt.querySelector("h2").textContent}
 Elements: ${elemnt.querySelector("ul").children.length}`
