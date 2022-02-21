ea9f22942245cc33b63c89d8b07aecbb7b953e10
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-02-21 23:04:57 +0400

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const creatingTag = function (element) {
+  const tag = document.createElement("li");
+  tag.textContent = element;
+  tag.classList.add("item");
+  return tag;
+};
+
+const ingredientsArr = ingredients.map(creatingTag);
+
+document.querySelector("#ingredients").append(...ingredientsArr);
