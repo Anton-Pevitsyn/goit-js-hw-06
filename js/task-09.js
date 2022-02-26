b164dfaaf2409c4b88b0969e5aebed3c6c07cbe1
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-02-26 19:24:53 +0400

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+const outputText = document.querySelector('.color')
+const body = document.querySelector('body')
+
+button.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor()
+outputText.textContent = getRandomHexColor()
+} )
